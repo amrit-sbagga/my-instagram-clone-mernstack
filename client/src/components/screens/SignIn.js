7ebdcc0f94337f4028593c31d527afcb455d9cf3
Amrit Singh <amrit.sbagga@gmail.com>
2020-12-28 21:29:56 +0530

    Added signup & signin screens

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Signup = () => {
+const SignIn = () => {
     return (
       <div className="mycard">     
         <div className="card auth-card input-field">
           <h2>Instagram</h2>
-          <input type="text" placeholder="name"/>
           <input type="text" placeholder="email"/>
           <input type="text" placeholder="password"/>
           <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
-            Signup
+            Login
           </button>
           <h5>
-            <Link to="/signin">Already have an account?</Link>
+            <Link to="/signup">Dont have an account?</Link>
           </h5>
         </div>
       </div>   
     )
 }
 
-export default Signup;
+export default SignIn;
