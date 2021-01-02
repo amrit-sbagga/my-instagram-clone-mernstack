f076869a6faacec19f8346414285954892667354
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-02 18:34:37 +0530

    Added toast msg and validation

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,28 +1,50 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import M from 'materialize-css';
 
 const Signup = () => {
+  const history = useHistory();
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
 
   const postData = () => {
+    //emailregex.com - added email regex pattern
+    if(!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))){
+      M.toast({
+        html: "Invalid email",
+        classes : "#c62828 red darken-3"
+      })
+      return;
+    }
       fetch("/signup",
-            {
-                method: "post",
-                headers : {
-                  "Content-Type" : "application/json"
-                },
-                body : JSON.stringify({
-                  name:"",
-                  password : "",
-                  email: ""
-                })
+        {
+            method: "post",
+            headers : {
+              "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({
+              name,
+              password,
+              email
+            })
         }).then(response => response.json())
         .then(data => {
+          if(data.error){
+            M.toast({
+              html: data.error,
+              classes : "#c62828 red darken-3"
+            })
+          } else {
+            M.toast({
+              html : data.msg,
+              classes : "#43a047 green darken-1"
+            })
+            history.push('/signin')
+          }
            console.log(data);
         })
-  }
+    }
 
     return (
       <div className="mycard">     
