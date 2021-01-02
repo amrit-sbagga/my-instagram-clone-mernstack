538cb819c0cf4ddd3d156f48c2c91cd3ad8f5989
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-02 17:52:55 +0530

    updated signup for network request

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const postData = () => {
+      fetch("http://localhost:5000/signup",
+            {
+                method: "post",
+                headers : {
+                  "Content-Type" : "application/json"
+                },
+                body : JSON.stringify({
+                  name:"",
+                  password : "",
+                  email: ""
+                })
+        }).then(response => response.json())
+        .then(data => {
+           console.log(data);
+        })
+  }
+
     return (
       <div className="mycard">     
         <div className="card auth-card input-field">
           <h2>Instagram</h2>
-          <input type="text" placeholder="name"/>
-          <input type="text" placeholder="email"/>
-          <input type="text" placeholder="password"/>
-          <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+          <input type="text" 
+                 placeholder="name"
+                 value={name}
+                 onChange = {(e) => setName(e.target.value)}/>
+          <input type="text" 
+                 placeholder="email"
+                 value={email}
+                 onChange = {(e) => setEmail(e.target.value)}/>
+          <input type="text" 
+                 placeholder="password"
+                 value={password}
+                 onChange = {(e) => setPassword(e.target.value)}/>
+          <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+             onClick = {() => postData()}>
             Signup
           </button>
           <h5>
             <Link to="/signin">Already have an account?</Link>
           </h5>
         </div>
-      </div>   
+      </div>    
     )
 }
 
