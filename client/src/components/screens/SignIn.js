462ee4b74071bd3bc075624d279e73535d005087
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-05 20:37:52 +0530

    Added context API

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import M from 'materialize-css';
+import { UserContext } from '../../App'
 
 const SignIn = () => {
+  const { state, dispatch } = useContext(UserContext);
   const history = useHistory();
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
@@ -36,6 +38,7 @@ const SignIn = () => {
           } else {
             localStorage.setItem("jwt", data.token);
             localStorage.setItem("user", JSON.stringify(data.user));
+            dispatch({type: "USER", payload: data.user});
             M.toast({
               html : "signedin success",
               classes : "#43a047 green darken-1"
