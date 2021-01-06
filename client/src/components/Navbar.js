3a8eb92134f6cae7bb28d00e059775dc9f1e56fe
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-06 18:10:29 +0530

    changed navigation links after signin and signout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../App'
 
 const NavBar = () => {
+
+    const { state, dispatch } = useContext(UserContext);
+    //initially state is empty
+    //to render links based on state
+    const renderList = () => {
+        if(state){
+            return [
+                <li><Link to="/profile">Profile</Link></li>,
+                <li><Link to="/create">Create Post</Link></li> 
+            ]
+        }else{
+            return [
+                <li><Link to="/signin">Signin</Link></li>,
+                <li><Link to="/signup">Signup</Link></li>
+            ]
+        }
+    }
+
     return (
         <nav>
             <div className="nav-wrapper white">
-            <Link to="/" className="brand-logo left">Instagram</Link>
+            <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><Link to="/signin">Signin</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/create">Create Post</Link></li>
+                {renderList()}
             </ul>
             </div>
         </nav>
